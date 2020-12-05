0d47c2c58d9251f0b80daf85a4dc7c13211b7fc5
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-05 04:38:15 -0800

    call for the day dec 4th. finished chapter 3.

---
+++ Exercise_Mortgage/src/com/toby/MortageCalculator.java
@@ -0,0 +1,10 @@
+package com.toby;
+
+import java.util.Scanner;
+
+public class MortageCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
