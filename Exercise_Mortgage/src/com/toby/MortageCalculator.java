c126e499f6de8855fba468eb6488b1dcbea316dd
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-06 04:11:39 -0800

    working on the mortgage calculator.

--- Exercise_Mortgage/src/com/toby/MortageCalculator.java
+++ Exercise_Mortgage/src/com/toby/MortageCalculator.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class MortageCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Welcome to your mortgage calculator. \nPlease type in the information...");
+
+        int Principle = 0;
+        while (Principle < 1000 || Principle > 1_000_000){
+            Principle = scanner.nextInt()
+        }
     }
 }
