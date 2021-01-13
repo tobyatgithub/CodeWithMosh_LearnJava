abb0a363bb47902fb65fe73328127bdb52ae9429
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-13 10:25:29 -0800

    Procedural Programming. The code is fat and need re-factor.

--- Part2_ObjectOrientedProgramming/Section2_Classes/src/com/toby/Main.java
+++ Part2_ObjectOrientedProgramming/Section2_Classes/src/com/toby/Main.java
@@ -9,5 +9,21 @@ public class Main {
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
         System.out.println(textBox1.text);
+
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
+
+        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        System.out.println(wage);
     }
+
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ){
+        return baseSalary + extraHours * hourlyRate;
+    }
+
 }
