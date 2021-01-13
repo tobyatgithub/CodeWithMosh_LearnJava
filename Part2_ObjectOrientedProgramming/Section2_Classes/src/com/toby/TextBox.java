87c2af200cda87ed94fdb59c3d2b3b4cdd0dd84b
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-12 23:07:19 -0800

    give variable a default value to avoid null pointer exception

--- Part2_ObjectOrientedProgramming/Section2_Classes/src/com/toby/TextBox.java
+++ Part2_ObjectOrientedProgramming/Section2_Classes/src/com/toby/TextBox.java
@@ -1,7 +1,8 @@
 package com.toby;
 
 public class TextBox {
-    public String text; // Field
+    //give variable a default value to avoid null pointer exception
+    public String text = ""; // Field
 
     public void setText(String text){
         this.text = text;
