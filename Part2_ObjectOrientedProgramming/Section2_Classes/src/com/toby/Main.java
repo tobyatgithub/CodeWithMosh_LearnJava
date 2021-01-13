0a4546c5203bde73627f4865586718fcc0d49a8b
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-12 21:07:41 -0800

    Create TextBox class in a separate file, and create a TextBox object in main.

---
+++ Part2_ObjectOrientedProgramming/Section2_Classes/src/com/toby/Main.java
@@ -0,0 +1,13 @@
+package com.toby;
+
+public class Main {
+    public static void main(String[] args) {
+        // Here we can use var or TextBox to declare this new variable
+        // var is cleaner and java will seek the type automatically
+        // (ps by using var, textBox1 is set to TextBox class and can't be
+        // changed into other type such as int/float/string etc.)
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
+    }
+}
