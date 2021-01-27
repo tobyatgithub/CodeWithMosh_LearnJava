c2b9b6eaf80fd1f03e857c88d79f4e5d2e3459e2
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-26 16:00:40 -0800

    3-the object class, and 4-constructors and inheritance.

--- Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
+++ Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
@@ -5,9 +5,7 @@ import org.w3c.dom.Text;
 public class Main {
 
   public static void main(String[] args) {
-//    var control = new UIControl();
-    var control = new TextBox();
-    control.disable();
-    System.out.println(control.isEnabled());
+    var textbox = new TextBox();
+//    System.out.println(textbox.isEnabled());
   }
 }
--- Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/TextBox.java
+++ Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/TextBox.java
@@ -3,6 +3,11 @@ package com.toby;
 public class TextBox extends UIControl{
   private String text = "";
 
+  public TextBox() {
+    super(true);
+    System.out.println("TextBox");
+  }
+
   public void setText(String text){
     this.text = text;
   }
