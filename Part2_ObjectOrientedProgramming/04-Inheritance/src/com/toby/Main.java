f8dc339a11f2bb7d6cad2b207029fd92034dadf1
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-26 16:14:01 -0800

    07 upcasting and downcasting.

--- Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
+++ Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
@@ -6,9 +6,25 @@ public class Main {
 
   public static void main(String[] args) {
     var textbox = new TextBox();
-//    System.out.println(textbox.isEnabled());
-    textbox.setText("Hello World");
-    System.out.println(textbox.toString());
-    System.out.println(textbox);
+//    textbox.setText("Hello World");
+//    System.out.println(textbox.toString());
+//    System.out.println(textbox);
+
+    var control = new UIControl(true);
+    show(control); // downcasting: UIControl -> TextBox
+
+    show(textbox); // upcasting: TextBox -> UIControl
+  }
+
+  public static void show(UIControl control){
+    // downcasting: you need to check instanceof
+    // when you are casting a more general object
+    // to a more specific object.
+    // i.e. without this check, show(control) will fail.
+    if (control instanceof TextBox){
+      var textbox = (TextBox) control;
+      textbox.setText("Hello World");
+    }
+    System.out.println(control);
   }
 }
