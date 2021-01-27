1879dee25cc04cb458fdd2b15dba1e1e70ac5ece
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-26 16:04:41 -0800

    5-access modifiers

--- Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/TextBox.java
+++ Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/TextBox.java
@@ -5,6 +5,7 @@ public class TextBox extends UIControl{
 
   public TextBox() {
     super(true);
+//    this.isEnabled; // the private field is not inherited
     System.out.println("TextBox");
   }
 
