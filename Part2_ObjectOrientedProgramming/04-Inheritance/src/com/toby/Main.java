75a663e046e1b11303181cd5ad5941c1cc2a40e9
tobyatgithub <toby.fangyuan@gmail.com>
2021-01-27 02:02:46 -0800

    08 - comparing objects, short cuts for overriding hash() and equals()

--- Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
+++ Part2_ObjectOrientedProgramming/04-Inheritance/src/com/toby/Main.java
@@ -5,6 +5,12 @@ import org.w3c.dom.Text;
 public class Main {
 
   public static void main(String[] args) {
+    var point1 = new Point(1,2);
+    var point2 = new Point(1,2);
+    System.out.println(point1.equals(point2));
+    System.out.println(point1.hashCode());
+    System.out.println(point2.hashCode());
+
     var textbox = new TextBox();
 //    textbox.setText("Hello World");
 //    System.out.println(textbox.toString());
